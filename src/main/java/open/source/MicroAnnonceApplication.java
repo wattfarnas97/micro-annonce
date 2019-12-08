b085d80733752115784def6ada70d8ba4af70804
unknown <wattfarouk@gmail.com>
2019-12-08 18:11:38 +0100

    firstCommit

---
+++ src/main/java/open/source/MicroAnnonceApplication.java
@@ -0,0 +1,13 @@
+package open.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAnnonceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAnnonceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/open/source/MicroAnnonceApplicationTests.java
@@ -0,0 +1,13 @@
+package open.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAnnonceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
